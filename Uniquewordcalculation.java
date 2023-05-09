50000811e345f114518dc0be548c72771a324419
Emaya009 <131105792+Emaya009@users.noreply.github.com>
2023-05-09 14:17:53 -0400

    Update Uniquewordcalculation.java

--- Uniquewordcalculation.java
+++ Uniquewordcalculation.java
@@ -8,7 +8,6 @@ import org.apache.logging.log4j.Logger;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.Iterator;
 
 public class Uniquewordcalculation {
     public static void main(String[] args) throws IOException
@@ -16,35 +15,40 @@ public class Uniquewordcalculation {
     {
         final Logger logger = LogManager.getLogger(String.valueOf(Uniquewordcalculation.class));
 
-        //Read text from file
-        File file=new File("src\\main\\java\\Petshop\\Inputfile.txt");
-        String text=FileUtils.readFileToString(file,"UTF-8");
-        text = text.toLowerCase();
-        text = text.replaceAll("[^a-z\\s]", "")
-                   .replace("\n", " ")
-                   .replace("\r", " ");
+        try {
 
-        //Split text into words
-        String[] words= StringUtils.split(text," ");
+            //Read text from file
+            File file = new File("src\\main\\java\\Petshop\\Inputfile.txt");
+            String text = FileUtils.readFileToString(file, "UTF-8");
+            text = text.toLowerCase();
+            text = text.replaceAll("[^a-z\\s]", "")
+                    .replace("\n", " ")
+                    .replace("\r", " ");
 
-        //Calculate number of unique words
-        HashSet<String> uniquewords=new HashSet<>();
-        for (String word : words)
-        {
-            uniquewords.add(word);
+            //Split text into words
+            String[] words = StringUtils.split(text, " ");
 
+            //Calculate number of unique words
+            HashSet<String> uniquewords = new HashSet<>();
+            for (String word : words) {
+                uniquewords.add(word);
+
+            }
+
+            int numUniqueWords = uniquewords.size();
+
+            //print result to console
+            logger.info("Number of unique words:" + numUniqueWords);
+            logger.info("List of unique words:" + uniquewords);
+
+            //write result to file
+            File outputfile = new File("src\\main\\java\\Petshop\\Outputfile.txt");
+            String result = "Number of unique words:" + numUniqueWords;
+            FileUtils.writeStringToFile(outputfile, result+ "\n" + "List of unique words are" +uniquewords, "UTF-8");
+
+            }catch (IOException e) {
+            logger.error("An error occurred.");
+            e.printStackTrace();
         }
-
-        int numUniqueWords=uniquewords.size();
-
-        //print result to console
-        logger.info("Number of unique words:"+numUniqueWords);
-        logger.info("Unique words:"+uniquewords);
-
-        //write result to file
-        File outputfile= new File("src\\main\\java\\Petshop\\Outputfile.txt");
-        String result="Number of unique words:" +numUniqueWords;
-        FileUtils.writeStringToFile(outputfile,result,"UTF-8");
-
     }
 }
